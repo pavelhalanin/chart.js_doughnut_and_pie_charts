1f3a8b6bd5403e11ce3f1c6333bfb3f112fc0f47
PavelHalanin <Pavel.Innokentevich.Galanin@gmail.com>
2025-05-13 23:38:08 +0300

    main: sources added

---
+++ js/main.js
@@ -0,0 +1,82 @@
+const TITLE = "Количество детей на круговой диаграмме";
+
+const COUNT_MAN_BEFORE_18 = 7;
+const COUNT_WOMAN_BEFORE_18 = 4;
+const COUNT_CHILDREN_BEFORE_18 = COUNT_MAN_BEFORE_18 + COUNT_WOMAN_BEFORE_18;
+
+const COUNT_MAN_AFTER_18 = 8;
+const COUNT_WOMAN_AFTER_18 = 2;
+const COUNT_CHILDREN_AFTER_18 = COUNT_MAN_AFTER_18 + COUNT_WOMAN_AFTER_18;
+
+const COUNT_ALL_CHILDREN = COUNT_CHILDREN_BEFORE_18 + COUNT_CHILDREN_AFTER_18;
+
+const IS_NO_CHILDREN = COUNT_ALL_CHILDREN == 0;
+
+const FIELDS = [
+  "Кол-во сыновей до 18 лет",
+  "Кол-во дочерей до 18 лет",
+  "Кол-во совершенолетних сыновей",
+  "Кол-во совершенолетних дочерей",
+  "Кол-во детей до 18 лет",
+  "Кол-во совершенолетних детей",
+  "Нет детей?",
+];
+
+const DATA = [
+  [
+    COUNT_MAN_BEFORE_18,
+    COUNT_WOMAN_BEFORE_18,
+    COUNT_MAN_AFTER_18,
+    COUNT_WOMAN_AFTER_18,
+    0,
+    0,
+    0,
+  ],
+  [0, 0, 0, 0, COUNT_CHILDREN_BEFORE_18, COUNT_CHILDREN_AFTER_18, 0],
+  [0, 0, 0, 0, 0, 0, IS_NO_CHILDREN],
+];
+
+const BACKGROUD_COLORS = [
+  "#4285F4",
+  "#f8a5c2",
+  "#8db600",
+  "#ff2626",
+  "gold",
+  "gray",
+  "#e5e5e5",
+];
+
+const coursesData = {
+  labels: FIELDS,
+  datasets: [
+    {
+      data: DATA[0],
+      backgroundColor: BACKGROUD_COLORS,
+    },
+    {
+      data: DATA[1],
+      backgroundColor: BACKGROUD_COLORS,
+    },
+    {
+      data: DATA[2],
+      backgroundColor: BACKGROUD_COLORS,
+    },
+  ],
+};
+
+const config = {
+  type: "doughnut",
+  data: coursesData,
+  options: {
+    plugins: {
+      title: {
+        display: true,
+        text: TITLE,
+      },
+    },
+  },
+};
+
+const ctx = document.getElementById("coursesDoughnutChart").getContext("2d");
+
+new Chart(ctx, config);
